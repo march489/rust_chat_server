bfb697f1d70b64829368e6e937d9cc4323a5f80e
Marcello Delgado <marcellodelgado0149@gmail.com>
2023-11-05 15:49:43 -0600

    Simple unit test for db calls complete

--- mdelga49-final/src/tests.rs
+++ mdelga49-final/src/tests.rs
@@ -5,10 +5,11 @@ use rocket::local::blocking::Client;
 use crate::post::Post;
 
 fn _run_test(base: &str, stage: AdHoc) {
-    const N: usize = 5;
+    const N: usize = 20;
 
     let client = Client::tracked(rocket::build().attach(stage)).unwrap();
 
+    // NOTE This is not threadsafe
     assert_eq!(client.delete(base).dispatch().status(), Status::Ok);
     assert_eq!(
         client.get(base).dispatch().into_json::<Vec<i32>>(),
@@ -18,7 +19,6 @@ fn _run_test(base: &str, stage: AdHoc) {
     for i in 1..=N {
         let mut post: Post = Post::new("MD", "Lobby", format!("This is post {i}").as_str());
 
-        println!("Trial {i}:");
         let response = client
             .post(base)
             .json(&mut post)
@@ -27,10 +27,10 @@ fn _run_test(base: &str, stage: AdHoc) {
 
         let response_id: i32 = response.unwrap().id.unwrap();
 
-        let list = client.get(base).dispatch().into_json::<Vec<i32>>().unwrap();
+        let list: Vec<i32> = client.get(base).dispatch().into_json::<Vec<i32>>().unwrap();
         assert_eq!(list.len(), i);
 
-        let last = list.last().unwrap();
+        let last: &i32 = list.last().unwrap();
         assert_eq!(*last, response_id);
 
         let response: Post = client
@@ -44,6 +44,18 @@ fn _run_test(base: &str, stage: AdHoc) {
         assert_eq!(response.thread, post.thread);
         assert_eq!(response.body, post.body);
     }
+
+    for _ in 1..=N {
+        let id_list: Vec<i32> = client.get(base).dispatch().into_json::<Vec<i32>>().unwrap();
+        assert!(id_list.len() > 0);
+
+        let id: &i32 = id_list.get(0).unwrap();
+        let response = client.delete(format!("{}/{}", base, id)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    let list: Vec<i32> = client.get(base).dispatch().into_json::<Vec<i32>>().unwrap();
+    assert!(list.is_empty());
 }
 
 #[test]
